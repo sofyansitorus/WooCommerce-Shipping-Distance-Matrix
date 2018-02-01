755ddef3ff9ee8df8746f2a734eced8da272aa8c
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-02-01 10:19:26 +0700

    Remove Maps Place Picker.

--- assets/js/wcsdm-admin.js
+++ assets/js/wcsdm-admin.js
@@ -1,12 +1,6 @@
 (function($) {
     "use strict";
     var wcsdmSetting = {
-        inputLatId: "woocommerce_wcsdm_origin_lat",
-        inputLngId: "woocommerce_wcsdm_origin_lng",
-        mapWrapperId: "wcsdm-map-wrapper",
-        mapSearchId: "wcsdm-map-search",
-        mapCanvasId: "wcsdm-map-canvas",
-        zoomLevel: 16,
         init: function() {
             var self = this;
             // Try show settings modal on settings page.
@@ -42,7 +36,6 @@
                     .find(".wc-shipping-zone-method-type")
                     .text() === wcsdm_params.method_title
                 ) {
-                    self._initGoogleMaps();
                     $("#woocommerce_wcsdm_gmaps_api_units").trigger("change");
                 }
             });
@@ -98,150 +91,6 @@
                 "#rates-list-table a.remove_rows",
                 self._removeRateRows
             );
-        },
-        _initGoogleMaps: function(apiKey) {
-            var self = this;
-            try {
-                if (
-                    typeof google === "undefined" ||
-                    typeof google.maps === "undefined"
-                ) {
-                    throw "google is not defined";
-                }
-
-                self._buildGoogleMaps();
-            } catch (error) {
-                $.getScript(
-                    "https://maps.googleapis.com/maps/api/js?key=&libraries=geometry,places",
-                    function() {
-                        self._buildGoogleMaps();
-                    }
-                );
-            }
-        },
-        _buildGoogleMaps: function() {
-            var self = this;
-            var curLat = $("#" + self.inputLatId).val();
-            var curLng = $("#" + self.inputLngId).val();
-            curLat = curLat.length ? parseFloat(curLat) : -6.175392;
-            curLng = curLng.length ? parseFloat(curLng) : 106.827153;
-            var curLatLng = {
-                lat: curLat,
-                lng: curLng
-            };
-            var tmplMapCanvas = wp.template(self.mapCanvasId);
-            var tmplMapSearch = wp.template(self.mapSearchId);
-            if (!$("#" + self.mapCanvasId).length) {
-                $("#" + self.mapWrapperId).append(
-                    tmplMapCanvas({
-                        map_canvas_id: self.mapCanvasId
-                    })
-                );
-            }
-            var markers = [];
-            var map = new google.maps.Map(document.getElementById(self.mapCanvasId), {
-                center: curLatLng,
-                zoom: self.zoomLevel,
-                mapTypeId: "roadmap"
-            });
-            var marker = new google.maps.Marker({
-                map: map,
-                position: curLatLng,
-                draggable: true,
-                icon: wcsdm_params.marker
-            });
-            var infowindow = new google.maps.InfoWindow({
-                maxWidth: 350,
-                content: wcsdm_params.txt.drag_marker
-            });
-            infowindow.open(map, marker);
-            google.maps.event.addListener(marker, "dragstart", function(event) {
-                infowindow.close();
-            });
-            google.maps.event.addListener(marker, "dragend", function(event) {
-                self._setLatLng(event.latLng, marker, map, infowindow);
-            });
-            markers.push(marker);
-            if (!$("#" + self.mapSearchId).length) {
-                $("#" + self.mapWrapperId).append(
-                    tmplMapSearch({
-                        map_search_id: self.mapSearchId
-                    })
-                );
-            }
-            // Create the search box and link it to the UI element.
-            var inputAddress = document.getElementById(self.mapSearchId);
-            var searchBox = new google.maps.places.SearchBox(inputAddress);
-            map.controls[google.maps.ControlPosition.TOP_LEFT].push(inputAddress);
-            // Bias the SearchBox results towards current map's viewport.
-            map.addListener("bounds_changed", function() {
-                searchBox.setBounds(map.getBounds());
-            });
-            // Listen for the event fired when the user selects a prediction and retrieve more details for that place.
-            searchBox.addListener("places_changed", function() {
-                var places = searchBox.getPlaces();
-                if (places.length === 0) {
-                    return;
-                }
-                // Clear out the old markers.
-                markers.forEach(function(marker) {
-                    marker.setMap(null);
-                });
-                markers = [];
-                // For each place, get the icon, name and location.
-                var bounds = new google.maps.LatLngBounds();
-                places.forEach(function(place) {
-                    if (!place.geometry) {
-                        console.log("Returned place contains no geometry");
-                        return;
-                    }
-                    marker = new google.maps.Marker({
-                        map: map,
-                        position: place.geometry.location,
-                        draggable: true,
-                        icon: wcsdm_params.marker
-                    });
-                    self._setLatLng(place.geometry.location, marker, map, infowindow);
-                    google.maps.event.addListener(marker, "dragstart", function(event) {
-                        infowindow.close();
-                    });
-                    google.maps.event.addListener(marker, "dragend", function(event) {
-                        self._setLatLng(event.latLng, marker, map, infowindow);
-                    });
-                    // Create a marker for each place.
-                    markers.push(marker);
-                    if (place.geometry.viewport) {
-                        // Only geocodes have viewport.
-                        bounds.union(place.geometry.viewport);
-                    } else {
-                        bounds.extend(place.geometry.location);
-                    }
-                });
-                map.setZoom(self.zoomLevel);
-                map.fitBounds(bounds);
-            });
-        },
-        _setLatLng: function(location, marker, map, infowindow) {
-            var self = this;
-            var geocoder = new google.maps.Geocoder();
-            geocoder.geocode({
-                    latLng: location
-                },
-                function(results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
-                        if (results[0]) {
-                            infowindow.setContent(results[0].formatted_address);
-                            infowindow.open(map, marker);
-                            $("#" + self.inputLatId).val(location.lat());
-                            $("#" + self.inputLngId).val(location.lng());
-                        } else {
-                            $("#" + self.inputLatId).val("");
-                            $("#" + self.inputLngId).val("");
-                        }
-                    }
-                }
-            );
-            map.setCenter(location);
         },
         _selectRateRows: function(e) {
             var elem = $(e.currentTarget);
--- includes/class-wcsdm.php
+++ includes/class-wcsdm.php
@@ -129,22 +129,22 @@ class Wcsdm extends WC_Shipping_Method {
 				'desc_tip'    => true,
 			),
 			'gmaps_api_key'            => array(
-				'title'       => __( 'API Key', 'wcsdm' ),
+				'title'       => __( 'Google Maps Distance Matrix API', 'wcsdm' ),
 				'type'        => 'text',
 				'description' => __( 'This plugin require Google Maps Distance Matrix API Services enabled in your Google Console. <a href="https://developers.google.com/maps/documentation/distance-matrix/get-api-key" target="_blank">Click here</a> to get API Key and to enable the services.', 'wcsdm' ),
 				'default'     => '',
 			),
-			'gmaps_address_picker'     => array(
-				'title' => __( 'Store Location', 'wcsdm' ),
-				'type'  => 'address_picker',
-			),
 			'origin_lat'               => array(
-				'type'    => 'hidden',
-				'default' => '',
+				'title'       => __( 'Store Location Latitude', 'wcsdm' ),
+				'type'        => 'decimal',
+				'description' => __( '<a href="http://www.latlong.net/" target="_blank">Click here</a> to get your store location coordinates info.', 'wcsdm' ),
+				'default'     => '',
 			),
 			'origin_lng'               => array(
-				'type'    => 'hidden',
-				'default' => '',
+				'title'       => __( 'Store Location Longitude', 'wcsdm' ),
+				'type'        => 'decimal',
+				'description' => __( '<a href="http://www.latlong.net/" target="_blank">Click here</a> to get your store location coordinates info.', 'wcsdm' ),
+				'default'     => '',
 			),
 			'gmaps_api_mode'           => array(
 				'title'       => __( 'Travel Mode', 'wcsdm' ),
@@ -209,69 +209,6 @@ class Wcsdm extends WC_Shipping_Method {
 			),
 		);
 	}
-
-	/**
-	 * Generate origin settings field.
-	 *
-	 * @since 1.2.4
-	 * @param string $key Settings field key.
-	 * @param array  $data Settings field data.
-	 */
-	public function generate_address_picker_html( $key, $data ) {
-		$field_key = $this->get_field_key( $key );
-
-		$defaults = array(
-			'title'             => '',
-			'disabled'          => false,
-			'class'             => '',
-			'css'               => '',
-			'placeholder'       => '',
-			'type'              => 'text',
-			'desc_tip'          => false,
-			'description'       => '',
-			'custom_attributes' => array(),
-			'options'           => array(),
-		);
-
-		$data = wp_parse_args( $data, $defaults );
-
-		ob_start(); ?>
-		<tr valign="top">
-			<th scope="row" class="titledesc">
-				<?php echo esc_html( $this->get_tooltip_html( $data ) ); ?>
-				<label for="<?php echo esc_attr( $field_key ); ?>"><?php echo wp_kses_post( $data['title'] ); ?></label>
-			</th>
-			<td class="forminp">
-				<div id="wcsdm-map-wrapper" class="wcsdm-map-wrapper"></div>
-				<script type="text/html" id="tmpl-wcsdm-map-search">
-					<input id="{{data.map_search_id}}" class="wcsdm-map-search controls" type="text" placeholder="<?php echo esc_attr( __( 'Search your store location', 'wcsdm' ) ); ?>" autocomplete="off" />
-				</script>
-				<script type="text/html" id="tmpl-wcsdm-map-canvas">
-					<div id="{{data.map_canvas_id}}" class="wcsdm-map-canvas"></div>
-				</script>
-			</td>
-		</tr>
-		<?php
-		return ob_get_clean();
-	}
-
-	/**
-	 * Generate hidden settings field.
-	 *
-	 * @since 1.2.4
-	 * @param string $key Settings field key.
-	 * @param array  $data Settings field data.
-	 */
-	public function generate_hidden_html( $key, $data ) {
-		$field_key = $this->get_field_key( $key );
-
-		ob_start();
-		?>
-		<input type="hidden" name="<?php echo esc_attr( $field_key ); ?>" id="<?php echo esc_attr( $field_key ); ?>" value="<?php echo esc_attr( $this->get_option( $key ) ); ?>">
-		<?php
-		return ob_get_clean();
-	}
-
 	/**
 	 * Generate table rates HTML form.
 	 *
