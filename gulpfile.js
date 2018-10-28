5ecf0144cb42fa1bb0864a76ea92e14dfb96c526
Sofyan Sitorus <sofyansitorus@gmail.com>
2018-10-29 06:05:30 +0700

    Improved gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var argv = require('yargs').argv;
 var gulpif = require('gulp-if');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
+var wpPot = require('gulp-wp-pot');
+var zip = require('gulp-zip');
 
 /**
  * Local variables
@@ -27,14 +29,14 @@ var scriptsDestDir = 'assets/js/';
 var stylesSrcDir = 'assets/src/scss/';
 var stylesDestDir = 'assets/css/';
 
-var sources = [
+var assets = [
     {
-        key: 'backend',
+        location: 'backend',
         scripts: ['helpers.js', 'map-picker.js', 'table-rates.js', 'backend.js'],
         styles: ['backend.scss'],
     },
     {
-        key: 'frontend',
+        location: 'frontend',
         scripts: ['helpers.js', 'frontend.js'],
         styles: ['frontend.scss'],
     }
@@ -55,14 +57,14 @@ var errorHandler = function () {
 /**
  * Script taks handler
  */
-var scriptsHandler = function (source, isMinify) {
-    var srcParam = source.scripts.map(function (file) {
+var scriptsHandler = function (asset, isMinify) {
+    var srcParam = asset.scripts.map(function (file) {
         return scriptsSrcDir + file;
     });
 
     return gulp.src(srcParam)
         .pipe(errorHandler())
-        .pipe(concat(source.key + '.js'))
+        .pipe(concat(asset.location + '.js'))
         .pipe(iife({
             useStrict: true,
             trimCode: true,
@@ -87,8 +89,8 @@ var scriptsHandler = function (source, isMinify) {
 /**
  * Style taks handler
  */
-var stylesHandler = function (source, isMinify) {
-    var srcParam = source.styles.map(function (file) {
+var stylesHandler = function (asset, isMinify) {
+    var srcParam = asset.styles.map(function (file) {
         return stylesSrcDir + file;
     });
 
@@ -125,14 +127,14 @@ var stylesHandler = function (source, isMinify) {
  */
 var tasksListBuild = [];
 
-sources.forEach(function (source) {
+assets.forEach(function (asset) {
     /**
      * Minify Scripts Task
      */
-    var scriptsTaskName = source.key + '-scripts-minify';
+    var scriptsTaskName = asset.location + '-scripts-minify';
 
     gulp.task(scriptsTaskName, function () {
-        return scriptsHandler(source, true);
+        return scriptsHandler(asset, true);
     });
 
     tasksListBuild.push(scriptsTaskName);
@@ -140,29 +142,36 @@ sources.forEach(function (source) {
     /**
      * Styles Task
      */
-    var stylesTaskName = source.key + '-styles-minify';
+    var stylesTaskName = asset.location + '-styles-minify';
 
     gulp.task(stylesTaskName, function () {
-        return stylesHandler(source, true);
+        return stylesHandler(asset, true);
     });
 
     tasksListBuild.push(stylesTaskName);
 });
-gulp.task('build', tasksListBuild);
+gulp.task('build', tasksListBuild, function () {
+    return gulp.src(['./*.php', './**/*.php'])
+        .pipe(wpPot({
+            'domain': 'wcsdm',
+            'package': 'WooCommerce-Shipping-Distance-Matrix'
+        }))
+        .pipe(gulp.dest('languages/wcsdm.pot'));
+});
 
 /**
  * Default tasks
  */
 var tasksListDefault = [];
 
-sources.forEach(function (source) {
+assets.forEach(function (asset) {
     /**
      * Scripts Task
      */
-    var scriptsTaskName = source.key + '-scripts';
+    var scriptsTaskName = asset.location + '-scripts';
 
     gulp.task(scriptsTaskName, function () {
-        return scriptsHandler(source, false);
+        return scriptsHandler(asset, false);
     });
 
     tasksListDefault.push(scriptsTaskName);
@@ -170,10 +179,10 @@ sources.forEach(function (source) {
     /**
      * Styles Task
      */
-    var stylesTaskName = source.key + '-styles';
+    var stylesTaskName = asset.location + '-styles';
 
     gulp.task(stylesTaskName, function () {
-        return stylesHandler(source, false);
+        return stylesHandler(asset, false);
     });
 
     tasksListDefault.push(stylesTaskName);
@@ -186,12 +195,19 @@ gulp.task('default', tasksListDefault, function () {
         });
     }
 
-    sources.forEach(function (source) {
-        var watchScriptsSrc = source.scripts.map(function (script) {
+    assets.forEach(function (asset) {
+        var watchScriptsSrc = asset.scripts.map(function (script) {
             return scriptsSrcDir + script;
         });
-        gulp.watch(watchScriptsSrc, [source.key + '-scripts']).on('change', browserSync.reload);
+        gulp.watch(watchScriptsSrc, [asset.location + '-scripts']).on('change', browserSync.reload);
 
-        gulp.watch(source.styles, [source.key + '-styles']);
+        gulp.watch(asset.styles, [asset.location + '-styles']);
     });
+});
+
+// Export task
+gulp.task('export', ['build'], function () {
+    gulp.src(['./**', '!dist/', '!dist/**', '!node_modules/', '!node_modules/**', '!assets/src/', '!assets/src/**', '!gulpfile.js', '!package-lock.json', '!package.json'])
+        .pipe(zip('wcsdm.zip'))
+        .pipe(gulp.dest('dist'));
 });
