fb2c1670fd7b913f12c7bdabe4cbdaa2bdd9fec3
Sofyan Sitorus <sofyansitorus@gmail.com>
2020-04-20 01:05:40 +0700

    Bumped version: 2.1.0

--- wcsdm.php
+++ wcsdm.php
@@ -15,7 +15,7 @@
  * Plugin Name:       WooReer
  * Plugin URI:        https://wooreer.com
  * Description:       WooCommerce shipping rates calculator that allows you to easily offer shipping rates based on the distance that calculated using Google Maps Distance Matrix Service API.
- * Version:           2.0.8
+ * Version:           2.1.0
  * Author:            Sofyan Sitorus
  * Author URI:        https://github.com/sofyansitorus
  * License:           GPL-2.0+
@@ -24,7 +24,7 @@
  * Domain Path:       /languages
  *
  * WC requires at least: 3.0.0
- * WC tested up to: 3.7.0
+ * WC tested up to: 4.0.1
  */
 
 // If this file is called directly, abort.
