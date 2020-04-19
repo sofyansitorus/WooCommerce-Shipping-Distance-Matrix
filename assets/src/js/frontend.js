b6906126cacbb2a4075d3afd37b670dbe60dbc18
Sofyan Sitorus <sofyansitorus@gmail.com>
2020-04-20 01:04:15 +0700

    Fixed JS error on the cart page
    Fixes: https://wordpress.org/support/topic/js-error-on-the-cart-page/

--- assets/src/js/frontend.js
+++ assets/src/js/frontend.js
@@ -1,54 +1,62 @@
 
 var wcsdmFrontendForm = {
-  setFields: function () {
-    wcsdmFrontendForm.fields = $.parseJSON(wc_address_i18n_params.locale.replace(/&quot;/g, '"'));
-  },
   bindEvents: function () {
     $(document.body).on('updated_wc_div updated_shipping_method', wcsdmFrontendForm.loadForm);
   },
   loadForm: function () {
-    _.each(wcsdmFrontendForm.getForms(), function (form) {
+    var forms = wcsdmFrontendForm.getForms();
+    var defaultFields = wcsdmFrontendForm.getDefaultFields();
+
+    _.each(forms, function (form) {
       var $wrapper = $(form.wrapper);
 
-      if ($wrapper && $wrapper.length) {
-        // Add address_1 & address_2 fields to calc_shipping form
-        if (form.prefix === 'calc_shipping' &&
-          (wcsdm_frontend.shipping_calculator_postcode || wcsdm_frontend.shipping_calculator_city)) {
-          var $cloneFieldWrap = $wrapper.find('#calc_shipping_city_field, #calc_shipping_postcode_field').first();
-
-          if ($cloneFieldWrap && $cloneFieldWrap.length) {
-            var addressFields = [];
-
-            if (wcsdm_frontend.shipping_calculator_address_1) {
-              addressFields.push('address_1');
-            }
-
-            if (wcsdm_frontend.shipping_calculator_address_2) {
-              addressFields.push('address_2');
-            }
-
-            _.each(addressFields, function (addressField) {
-              var $addresFieldWrap = $cloneFieldWrap.clone().attr({
-                id: 'calc_shipping_' + addressField + '_field'
-              });
-
-              $addresFieldWrap.find('input').attr({
-                id: 'calc_shipping_' + addressField,
-                name: 'calc_shipping_' + addressField,
-                placeholder: wcsdmFrontendForm.fields['default'][addressField].placeholder,
-                value: $('#wcsdm-calc-shipping-field-value-' + addressField).val()
-              }).trigger('change');
-
-              $cloneFieldWrap.before($addresFieldWrap);
-
-              $('#wcsdm-calc-shipping-field-value-' + addressField).remove();
-            });
-          }
-        }
-
-        $(document.body).trigger('wcsdm_form_loaded_' + form.prefix, form);
-        $(document.body).trigger('wcsdm_form_loaded', form);
+      if (!$wrapper || !$wrapper.length) {
+        return;
       }
+
+      // Add address_1 & address_2 fields to calc_shipping form
+      if (form.prefix === 'calc_shipping') {
+        var $fieldToClone = $wrapper.find('#calc_shipping_city_field, #calc_shipping_postcode_field').first();
+
+        if ($fieldToClone && $fieldToClone.length) {
+          _.each(defaultFields, function (fieldData, fieldKey) {
+            if (!wcsdm_frontend['shipping_calculator_' + fieldKey]) {
+              return;
+            }
+
+            if (!_.contains(['address_1', 'address_2'], fieldKey)) {
+              return;
+            }
+
+            if ($wrapper.find('#calc_shipping_' + fieldKey + '_field').length) {
+              return;
+            }
+
+            var $fieldCloned = $fieldToClone.clone().attr({
+              id: 'calc_shipping_' + fieldKey + '_field',
+            });
+
+            var placeholder = fieldData.placeholder || '';
+
+            $fieldCloned.find('input').attr({
+              'id': 'calc_shipping_' + fieldKey,
+              'name': 'calc_shipping_' + fieldKey,
+              'value': $('#wcsdm-calc-shipping-field-value-' + fieldKey).val(),
+              'placeholder': placeholder,
+              'data-placeholder': placeholder,
+            }).data({
+              'placeholder': placeholder,
+            }).trigger('change');
+
+            $fieldToClone.before($fieldCloned);
+
+            $('#wcsdm-calc-shipping-field-value-' + fieldKey).remove();
+          });
+        }
+      }
+
+      $(document.body).trigger('wcsdm_form_loaded_' + form.prefix, form);
+      $(document.body).trigger('wcsdm_form_loaded', form);
     });
   },
   getForms: function () {
@@ -63,18 +71,14 @@ var wcsdmFrontendForm = {
       prefix: 'calc_shipping'
     }];
   },
+  getDefaultFields: function () {
+    try {
+      return JSON.parse(wc_address_i18n_params.locale).default;
+    } catch (error) {
+      console.log('getDefaultFields', error);
+    }
+  },
   init: function () {
-    // wc_address_i18n_params is required to continue, ensure the object exists
-    if (typeof wc_address_i18n_params === 'undefined') {
-      return false;
-    }
-
-    // wc_country_select_params is required to continue, ensure the object exists
-    if (typeof wc_country_select_params === 'undefined') {
-      return false;
-    }
-
-    wcsdmFrontendForm.setFields();
     wcsdmFrontendForm.bindEvents();
     wcsdmFrontendForm.loadForm();
   }
