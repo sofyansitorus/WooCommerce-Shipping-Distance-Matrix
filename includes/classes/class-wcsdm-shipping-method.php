a73bfabb132d2d8af3547eca44eab38e119e5381
Sofyan Sitorus <sofyansitorus@gmail.com>
2026-03-10 19:08:00 +0000

    Use wcsdm_is_empty_string function to validate value in Wcsdm_Shipping_Method class

--- includes/classes/class-wcsdm-shipping-method.php
+++ includes/classes/class-wcsdm-shipping-method.php
@@ -2932,7 +2932,7 @@ class Wcsdm_Shipping_Method extends WC_Shipping_Method {
 
 		$value = $rate_row[ $key ] ?? $field['default'] ?? '';
 
-		if ( ! is_null( $empty_value ) && '' === $value ) {
+		if ( ! is_null( $empty_value ) && wcsdm_is_empty_string( $value ) ) {
 			$value = $empty_value;
 		}
 
